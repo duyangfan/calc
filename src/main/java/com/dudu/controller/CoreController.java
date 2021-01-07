4893bc95dc4b90c7c111c44344c1be1fd9a9ab83
duyangfan <599588568@qq.com>
2021-01-07 14:45:05 +0800

    init  ..

---
+++ src/main/java/com/dudu/controller/CoreController.java
@@ -0,0 +1,13 @@
+package com.dudu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("core")
+public class CoreController {
+
+
+
+
+}
